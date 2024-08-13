8be3fd334be9dbb5e905e9b7d58f4067ad4d4349
barczynsky <barczynsky@users.noreply.github.com>
2024-08-13 11:44:12 +0200

    moved common code into `podman/.config` source script

--- podman/create.sh
+++ podman/create.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -eu
 
-# --------------------------------------------------------------------------
-
-sudont() { test -n "${SUDO_UID:-}" && setpriv --reuid "${SUDO_UID}" --regid "${SUDO_GID}" --keep-groups "$@" || "$@"; }
-
-# --------------------------------------------------------------------------
-
-trs() { sed 's/^\s*//;s/\s*$//;s/\s\+/_/g'; }
-Valheim_IMAGE_ID="$(trs <<<"${Valheim_IMAGE_ID:-1}")"
-test -z "${Valheim_CONTAINER_ID:-}" && Valheim_CONTAINER_ID="${Valheim_IMAGE_ID}" ||
-Valheim_CONTAINER_ID="$(trs <<<"${Valheim_CONTAINER_ID:-1}")"
+. podman/.config
 
 # --------------------------------------------------------------------------
 
--- podman/stop.sh
+++ podman/stop.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -eu
 
-# --------------------------------------------------------------------------
-
-trs() { sed 's/^\s*//;s/\s*$//;s/\s\+/_/g'; }
-Valheim_IMAGE_ID="$(trs <<<"${Valheim_IMAGE_ID:-1}")"
-test -z "${Valheim_CONTAINER_ID:-}" && Valheim_CONTAINER_ID="${Valheim_IMAGE_ID}" ||
-Valheim_CONTAINER_ID="$(trs <<<"${Valheim_CONTAINER_ID:-1}")"
+. podman/.config
 
 # --------------------------------------------------------------------------
 
